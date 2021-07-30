f8cb255210c80473929188cac1b4a4f295d4ae4b
Imon <imon62121@gmail.com>
2021-07-30 22:04:10 +0600

    dont know

--- src/pages/Chapter.js
+++ src/pages/Chapter.js
@@ -7,133 +7,248 @@ import {
   CardActions,
   CardContent,
   CardMedia,
-  Button
+  Button,
+  IconButton,
 } from "@material-ui/core";
 import React from "react";
 
-const dataModel = {
-  primary: {
-    name: "1st paper",
-    chapter: [
-      {
-        chapter: "1st chapter",
-        name: "nomenclature",
-        class_started: "14 nov 22",
-        total: 17,
-        running: false
-      },
-      {
-        chapter: "2nd chapter",
-        name: "strichiometry",
-        class_started: "14 nov 22",
-        total: 17,
-        running: true
-      },
-      {
-        chapter: "3rd chapter",
-        name: "nomenclature",
-        class_started: "14 nov 22",
-        total: 17,
-        running: false
-      },
-      {
-        chapter: "4th chapter",
-        name: "strichiometry",
-        class_started: "14 nov 22",
-        total: 17,
-        running: false
-      }
-    ]
-  },
-  Secondary: {
-    name: "2nd paper",
-    chapter: [
-      {
-        chapter: "1st chapter",
-        name: "nomenclature",
-        class_started: "14 nov 22",
-        total: 17,
-        running: true
-      },
-      {
-        chapter: "2nd chapter",
-        name: "strichiometry",
-        class_started: "14 nov 22",
-        total: 17,
-        running: false
-      },
-      {
-        chapter: "3rd chapter",
-        name: "nomenclature",
-        class_started: "14 nov 22",
-        total: 17,
-        running: false
-      },
-      {
-        chapter: "4th chapter",
-        name: "strichiometry",
-        class_started: "14 nov 22",
-        total: 17,
-        running: false
-      }
-    ],
-    class_started: "24 feb 23"
-  }
-};
+// adding icon
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 
 const classes = makeStyles((theme) => ({
   cardRoot: {
-    maxWidth: 345
-  }
+    maxWidth: 345,
+  },
 }));
 
-const List = (props) => {
-  return (
-    <>
-      <Typography variant="h4" gutterBottom>
-        {props.data.name}
-      </Typography>
-      <Grid container spacing={4}>
-        <Grid item xs={0} md={1} />
-        <Grid item container spacing={3} xs={12} md={10}>
-          {props.data.chapter.map((x) => {
-            return (
-              <Grid item xs={10} md={3}>
-                <Card className={classes.cardRoot}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {x.chapter + " - " + x.name}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        Class Started: {x.class_started}
-                        <br />
-                        Total Class: {x.total}
-                        <br />
-                        Status: {x.running ? "Running" : "Finished"}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
-        <Grid item xs={0} md={1} />
-      </Grid>
-    </>
-  );
-};
-
 export default function Chapter(props) {
+  // login data
+  const [show, setShow] = React.useState(true);
+  // test data
+  const [dataModel, setDataModel] = React.useState({
+    primary: {
+      name: "1st paper",
+      chapter: [
+        {
+          id: 1,
+          no: 1,
+          chapter: "1st chapter",
+          name: "nomenclature",
+          class_started: "14 nov 22",
+          total: 17,
+          running: false,
+        },
+        {
+          id: 2,
+          no: 2,
+          chapter: "2nd chapter",
+          name: "strichiometry",
+          class_started: "14 nov 22",
+          total: 17,
+          running: true,
+        },
+        {
+          id: 3,
+          no: 3,
+          chapter: "3rd chapter",
+          name: "nomenclature",
+          class_started: "14 nov 22",
+          total: 17,
+          running: false,
+        },
+        {
+          id: 4,
+          no: 4,
+          chapter: "4th chapter",
+          name: "strichiometry",
+          class_started: "14 nov 22",
+          total: 17,
+          running: false,
+        },
+      ],
+    },
+    secondary: {
+      name: "2nd paper",
+      chapter: [
+        {
+          id: 5,
+          no: 5,
+          chapter: "1st chapter",
+          name: "nomenclature",
+          class_started: "14 nov 22",
+          total: 17,
+          running: true,
+        },
+        {
+          id: 6,
+          no: 6,
+          chapter: "2nd chapter",
+          name: "strichiometry",
+          class_started: "14 nov 22",
+          total: 17,
+          running: false,
+        },
+        {
+          id: 7,
+          no: 7,
+          chapter: "3rd chapter",
+          name: "nomenclature",
+          class_started: "14 nov 22",
+          total: 17,
+          running: false,
+        },
+        {
+          id: 8,
+          no: 8,
+          chapter: "4th chapter",
+          name: "strichiometry",
+          class_started: "14 nov 22",
+          total: 17,
+          running: false,
+        },
+      ],
+    },
+  });
+  // sort function
+  const sorter = (arr) => {
+    arr.sort((a, b) => {
+      return a.no - b.no;
+    });
+    return arr;
+  };
+  // create function
+  const create = () => {};
+  // edit function
+  const edit = (id) => {
+    dataModel.primary.chapter.map((x) => {
+      if (x.id === id) {
+        alert(x.running);
+      }
+    });
+    if (dataModel.secondary) {
+      dataModel.secondary.chapter.map((x) => {
+        if (x.id === id) {
+          alert(x.running);
+        }
+      });
+    }
+  };
+  // remove function
+  const remove = (id) => {
+    const temp = dataModel.primary.chapter.filter((x) => {
+      return x.id !== id;
+    });
+    const data = dataModel;
+    data.primary.chapter = temp;
+    setDataModel({ ...data });
+    console.log(dataModel);
+    if (dataModel.secondary) {
+      const sectemp = dataModel.secondary.chapter.filter((x) => {
+        return x.id !== id;
+      });
+      const secdata = dataModel;
+      secdata.secondary.chapter = sectemp;
+      setDataModel({ ...secdata });
+    }
+  };
+  const List = (props) => {
+    return (
+      <>
+        <Typography variant="h4" gutterBottom>
+          {props.data.name}
+        </Typography>
+        <Grid container spacing={4}>
+          <Grid item xs={0} md={1} />
+          <Grid item container spacing={3} xs={12} md={10}>
+            {sorter(props.data.chapter).map((x) => {
+              return (
+                <Grid item xs={10} md={4} lg={3} key={x.id}>
+                  <Card className={classes.cardRoot}>
+                    <CardActionArea>
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {x.chapter + " - " + x.name}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          Class Started: {x.class_started}
+                          <br />
+                          Total Class: {x.total}
+                          <br />
+                          Status: {x.running ? "Running" : "Finished"}
+                          <br />
+                          {show ? (
+                            <>
+                              <IconButton
+                                onClick={() => {
+                                  setShow(!show);
+                                }}
+                              >
+                                <EditIcon color="secondary" />
+                              </IconButton>
+                              <IconButton
+                                onClick={() => {
+                                  remove(x.id);
+                                }}
+                              >
+                                <DeleteIcon color="error" />
+                              </IconButton>
+                            </>
+                          ) : (
+                            ""
+                          )}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+          <Grid item xs={0} md={1} />
+        </Grid>
+      </>
+    );
+  };
   return (
     <>
       <List data={dataModel.primary} />
-      <List data={dataModel.Secondary} />
+      {show ? (
+        <CardActionArea>
+          <Typography variant="h5" color="secondary" gutterBottom>
+            <IconButton>
+              <AddCircleIcon color="secondary" />
+            </IconButton>
+            Add class group
+          </Typography>
+        </CardActionArea>
+      ) : (
+        ""
+      )}
+      {dataModel.secondary ? (
+        <>
+          <List data={dataModel.secondary} />
+          {show ? (
+            <CardActionArea>
+              <Typography variant="h5" color="secondary" gutterBottom>
+                <IconButton>
+                  <AddCircleIcon color="secondary" />
+                </IconButton>
+                Add class group
+              </Typography>
+            </CardActionArea>
+          ) : (
+            ""
+          )}
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 }
--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+// import ing material component
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import { makeStyles } from "@material-ui/core/styles";
+
+import {} from "@material-ui/core";
+
 const data = {
   classes: [
     {
@@ -7,32 +20,86 @@ const data = {
       name: "basic class",
       link: "www.test.com",
       linkText: "click me",
-      date: "12 jun 21"
+      date: "12 jun 21",
     },
     {
       no: 2,
       name: "2nos class",
       link: "www.test.com",
       linkText: "click me",
-      date: "12 jun 21"
+      date: "12 jun 21",
     },
     {
       no: 3,
       name: "basic class",
       link: "www.test.com",
       linkText: "click me",
-      date: "12 jun 21"
+      date: "12 jun 21",
     },
     {
       no: 4,
       name: "2nos class",
       link: "www.test.com",
       linkText: "click me",
-      date: "12 jun 21"
-    }
-  ]
+      date: "12 jun 21",
+    },
+  ],
 };
 
+const columns = [
+  { id: "no", label: "No.", minWidth: 50 },
+  { id: "name", label: "Topics", minWidth: 150 },
+  { id: "date", label: "Date", minWidth: 60 },
+  { id: "link", label: "Links", minWidth: 50 },
+];
+
+const classes = makeStyles({
+  root: {
+    width: "100%",
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
 export default function Classes() {
-  return <>hello world</>;
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.classes.map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.no}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === "number"
+                          ? column.format(value)
+                          : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 }

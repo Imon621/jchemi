f872b689bfffd2f0e25b228b83028fd0c0798fc8
Imon <imon62121@gmail.com>
2021-08-03 19:57:37 +0600

    successfully created firebase relation

---
+++ src/components/firebase.js
@@ -0,0 +1,5 @@
+import firebase from "./fireConfig";
+
+const db = firebase.firestore();
+
+export default db;
--- src/pages/Chapter.js
+++ src/pages/Chapter.js
@@ -4,9 +4,12 @@ import {
   makeStyles,
   Card,
   CardActionArea,
+  CircularProgress,
   CardContent,
 } from "@material-ui/core";
 import React from "react";
+
+import db from "../components/firebase";
 
 const classes = makeStyles((theme) => ({
   cardRoot: {
@@ -15,93 +18,127 @@ const classes = makeStyles((theme) => ({
 }));
 
 export default function Chapter(props) {
-  // login data
+  // data model
+  const dataModel = [
+    {
+      year: "primary",
+      id: 1,
+      no: 1,
+      chapter: "1st chapter",
+      name: "nomenclature",
+      class_started: "14 nov 22",
+      total: 17,
+      running: false,
+    },
+    {
+      year: "primary",
+      id: 2,
+      no: 2,
+      chapter: "2nd chapter",
+      name: "strichiometry",
+      class_started: "14 nov 22",
+      total: 17,
+      running: true,
+    },
+    {
+      year: "primary",
+      id: 3,
+      no: 3,
+      chapter: "3rd chapter",
+      name: "nomenclature",
+      class_started: "14 nov 22",
+      total: 17,
+      running: false,
+    },
+    {
+      year: "primary",
+      id: 4,
+      no: 4,
+      chapter: "4th chapter",
+      name: "strichiometry",
+      class_started: "14 nov 22",
+      total: 17,
+      running: false,
+    },
+    {
+      year: "secondary",
+      id: 5,
+      no: 5,
+      chapter: "1st chapter",
+      name: "nomenclature",
+      class_started: "14 nov 22",
+      total: 17,
+      running: true,
+    },
+    {
+      year: "secondary",
+      id: 6,
+      no: 6,
+      chapter: "2nd chapter",
+      name: "strichiometry",
+      class_started: "14 nov 22",
+      total: 17,
+      running: false,
+    },
+    {
+      year: "secondary",
+      id: 7,
+      no: 7,
+      chapter: "3rd chapter",
+      name: "nomenclature",
+      class_started: "14 nov 22",
+      total: 17,
+      running: false,
+    },
+    {
+      year: "secondary",
+      id: 8,
+      no: 8,
+      chapter: "4th chapter",
+      name: "strichiometry",
+      class_started: "14 nov 22",
+      total: 17,
+      running: false,
+    },
+  ];
 
   // test data
-  const [dataModel, setDataModel] = React.useState({
-    primary: {
-      name: "1st paper",
-      chapter: [
-        {
-          id: 1,
-          no: 1,
-          chapter: "1st chapter",
-          name: "nomenclature",
-          class_started: "14 nov 22",
-          total: 17,
-          running: false,
-        },
-        {
-          id: 2,
-          no: 2,
-          chapter: "2nd chapter",
-          name: "strichiometry",
-          class_started: "14 nov 22",
-          total: 17,
-          running: true,
-        },
-        {
-          id: 3,
-          no: 3,
-          chapter: "3rd chapter",
-          name: "nomenclature",
-          class_started: "14 nov 22",
-          total: 17,
-          running: false,
-        },
-        {
-          id: 4,
-          no: 4,
-          chapter: "4th chapter",
-          name: "strichiometry",
-          class_started: "14 nov 22",
-          total: 17,
-          running: false,
-        },
-      ],
-    },
-    secondary: {
-      name: "2nd paper",
-      chapter: [
-        {
-          id: 5,
-          no: 5,
-          chapter: "1st chapter",
-          name: "nomenclature",
-          class_started: "14 nov 22",
-          total: 17,
-          running: true,
-        },
-        {
-          id: 6,
-          no: 6,
-          chapter: "2nd chapter",
-          name: "strichiometry",
-          class_started: "14 nov 22",
-          total: 17,
-          running: false,
-        },
-        {
-          id: 7,
-          no: 7,
-          chapter: "3rd chapter",
-          name: "nomenclature",
-          class_started: "14 nov 22",
-          total: 17,
-          running: false,
-        },
-        {
-          id: 8,
-          no: 8,
-          chapter: "4th chapter",
-          name: "strichiometry",
-          class_started: "14 nov 22",
-          total: 17,
-          running: false,
-        },
-      ],
-    },
-  });
+  const [chapter, setChapter] = React.useState("");
+  // fetching data
+  React.useEffect(() => {
+    db.collection("courses")
+      .doc("master")
+      .collection("chapter")
+      .get()
+      .then((x) => {
+        const arr = [];
+        x.docs.map((doc) => {
+          const obj = {
+            year: doc.data().year,
+            id: doc.id,
+            no: doc.data().no,
+            chapter: doc.data().chapter,
+            class_started: doc.data().class_started,
+            name: doc.data().name,
+            total: doc.data().total,
+            running: doc.data().running,
+            tags: doc.data().tags,
+          };
+          arr.push(obj);
+        });
+        setChapter(arr);
+      });
+  }, []);
+  // year filtering data
+  const filt = (year) => {
+    const arr = [];
+    chapter.map((x) => {
+      if (x.year === year) {
+        arr.push(x);
+      }
+    });
+    return arr;
+  };
   // sort function
   const sorter = (arr) => {
     arr.sort((a, b) => {
@@ -114,12 +151,12 @@ export default function Chapter(props) {
     return (
       <>
         <Typography variant="h4" gutterBottom>
-          {props.data.name}
+          {props.data[0].year === "primary" ? "1st Year" : "2nd Year"}
         </Typography>
         <Grid container spacing={4}>
           <Grid item xs={0} md={1} />
           <Grid item container spacing={3} xs={12} md={10}>
-            {sorter(props.data.chapter).map((x) => {
+            {sorter(props.data).map((x) => {
               return (
                 <Grid item xs={10} md={4} lg={3} key={x.id}>
                   <Card className={classes.cardRoot}>
@@ -155,13 +192,22 @@ export default function Chapter(props) {
   };
   return (
     <>
-      <List data={dataModel.primary} />
-      {dataModel.secondary ? (
+      {chapter !== "" ? (
         <>
-          <List data={dataModel.secondary} />
+          <List data={filt("primary")} />
+          {filt("secondary") ? <List data={filt("secondary")} /> : ""}
         </>
       ) : (
-        ""
+        <div
+          style={{
+            height: 70 + "vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress style={{}} />
+        </div>
       )}
     </>
   );
